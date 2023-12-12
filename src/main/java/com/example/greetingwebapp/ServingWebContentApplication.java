0396b3d2fb79e55074d9e8ce697fdb9c3febf77d
Jaashee <jashakan2000@gmail.com>
2023-12-12 16:01:07 -0500

    Final Commit B4 Submission

--- src/main/java/com/example/greetingwebapp/ServingWebContentApplication.java
+++ src/main/java/com/example/greetingwebapp/ServingWebContentApplication.java
@@ -1,2 +1,13 @@
-package com.example.greetingwebapp;public class ServingWebContentApplication {
+package com.example.greetingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
 }
